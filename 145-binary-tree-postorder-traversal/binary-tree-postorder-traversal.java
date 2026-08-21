6cd05dadb65533a21a3cb061106fd37d3e62ba24
HarshSrivastava18 <harshsrivastava1811@gmail.com>
2026-08-21 09:53:31 +0530

    Time: 1 ms (11.89%) | Memory: 43 MB (71.49%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -15,42 +15,31 @@
  */
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
-        java.util.List<Integer> postorder = new java.util.ArrayList<>();  // List to store the postorder traversal
-        
-        // If the tree is empty, return an empty traversal
-        if (root == null) {
+        List<Integer> postorder= new LinkedList<Integer>();
+
+       Stack<TreeNode> st1 = new Stack<>();
+Stack<TreeNode> st2 = new Stack<>();
+        if(root==null){
             return postorder;
         }
-
-        java.util.Stack<TreeNode> st1 = new java.util.Stack<>();  // First stack for iterative traversal
-        java.util.Stack<TreeNode> st2 = new java.util.Stack<>();  // Second stack to store the nodes in postorder
-
-        // Push the root node onto the first stack
         st1.push(root);
+        while(!st1.isEmpty()){
+            root=st1.pop();
+            st2.push(root);
 
-        // Iterative traversal to populate st2 with nodes in postorder
-        while (!st1.isEmpty()) {
-            root = st1.pop();  // Get the top node from st1
-            st2.push(root);  // Push the node onto st2
-
-            // Push left child onto st1 if exists
-            if (root.left != null) {
+             if (root.left != null) {
                 st1.push(root.left);
             }
 
-            // Push right child onto st1 if exists
             if (root.right != null) {
                 st1.push(root.right);
             }
         }
-
-        // Populate the postorder traversal list by popping st2
-        while (!st2.isEmpty()) {
-            postorder.add(st2.pop().val);  // Add the node's value to the postorder result
+            while (!st2.isEmpty()) {
+            postorder.add(st2.pop().val);  
         }
 
-        // Return the postorder traversal result
         return postorder;
-    } 
-    
+
+    }
 }
